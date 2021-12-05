09f16db76000b26abae5e34de681fdfab8ee8318
priyanshu Chaudhary <priyanshuchaudhary@priyanshus-MacBook-Pro.local>
2021-12-05 19:00:54 +0530

    fixed login form

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -6,16 +6,24 @@ import { Link } from "react-router-dom";
 export default function Login() {
     const [user, setUser] = useState('')
     const [password, setPassword] = useState('')
-    const login=(e)=>{
-        axios({
-            method: "post",
-            data:{
-                usernmae: user,
-                password: password
-            },
-            withCredentials: true,
-            url: "https://tour-explore.herokuapp.com/login"
-        })
+    const err = document.querySelector('.loginerror')
+    const login=()=>{
+        if(!user.length || !password.length)
+        {
+            err.innerHTML = 'Correct username and password are required'
+            err.style.color = "red"
+        }
+        else{
+            axios({
+                method: "POST",
+                data:{
+                    username: user,
+                    password: password
+                },
+                withCredentials: true,
+                url: "https://tour-explore.herokuapp.com/login"
+            })
+        }
     }
 
     return (
@@ -26,7 +34,8 @@ export default function Login() {
                         <img className="card-img-top" src="https://images.unsplash.com/photo-1508672019048-805c876b67e2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1838&q=80" alt="" />
                         <h5 className="text-center">Login</h5>
                         <div className="col-6 offset-3">
-                            <form className="needs-validation">
+                            {/* <form className="needs-validation loginform"> */}
+                                <p className="loginerror"></p>
                                 <div className="mb-3">
                                     <label className="form-label">Username</label>
                                     <input className="form-control" onChange={(e)=> setUser(e.target.value)} type="text" id="username" autoFocus required />
@@ -38,7 +47,7 @@ export default function Login() {
                                 <div className="d-grid gap-2">
                                 <button className="btn btn-success mb-4" onClick={login} >Login</button>
                                 </div>
-                            </form>
+                            {/* </form> */}
                             <Link className="align-center btn btn-secondary mb-4 " to="/campgrounds">Go Back</Link>
                         </div>
                     </div>
